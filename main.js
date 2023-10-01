ffe2016f7c71948cfd8adb731273585d40e4b51a
js8cr3fk9 <87287151+js8cr3@users.noreply.github.com>
2023-09-30 22:00:16 -0400

    lalal

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const symbolA = settings.symbolA;
 const gridUnit = settings.gridUnit;
 
 log(monsterData);
-const monsterBoundary = getMonsterBoundary(monsterData[0], monsterData[1]);
+//const monsterBoundary = getMonsterBoundary(...monsterData);
+const monsterBoundary = getMonsterBoundary('wolfie', 'winterland');
 monsterBoundary[0] -= gridUnit * 2;
 monsterBoundary[2] += gridUnit * 4;
 
